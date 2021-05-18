b72b4946a61404e436c4d6f5cec4d7b74e3e69cd
RISK\FernKE02 <kellen.fernandez@lexisnexisrisk.com>
2021-05-18 07:29:44 -0300

    Second Class -> First it was only configuration

---
+++ src/main/java/br/com/ChronosAcademy/LogicaProgramacao/HellowWorld.java
@@ -0,0 +1,13 @@
+package br.com.ChronosAcademy.LogicaProgramacao;
+
+import java.sql.SQLOutput;
+
+public class HellowWorld {
+
+    // Classe que cria o exe do Java:
+    public static void main(String[] args) {
+        //println quebra uma linha.
+        System.out.println("Hello World");
+
+    }
+}
