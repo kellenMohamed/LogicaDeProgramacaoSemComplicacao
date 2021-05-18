a6d441161a6665a15657e4ab8d777528094dfc46
RISK\FernKE02 <kellen.fernandez@lexisnexisrisk.com>
2021-05-18 07:48:40 -0300

    added some variables to test the variable types.

--- src/main/java/br/com/ChronosAcademy/LogicaProgramacao/HellowWorld.java
+++ src/main/java/br/com/ChronosAcademy/LogicaProgramacao/HellowWorld.java
@@ -9,5 +9,31 @@ public class HellowWorld {
         //println quebra uma linha.
         System.out.println("Hello World");
 
+        //Teste tipos de variáveis;
+        boolean encontrado = false;
+        encontrado = true;
+        System.out.println(encontrado);
+
+        char varChar = 'A';
+        System.out.println("Char: " + varChar);
+
+        float varFloat = 12.5f;
+        System.out.println("Float: " + varFloat);
+
+        double varDouble = 12.5;
+        System.out.println("Double: " + varDouble);
+
+        byte varByte = 127;
+        System.out.println("Byte: " + varByte);
+
+        short varShort = 32767;
+        System.out.println("Short: " + varShort);
+
+        int varInt = 2147483647;
+        System.out.println("Int: " + varInt);
+
+        long varLong = 922337206854775806L;
+        System.out.println("Long: " + varLong);
+
     }
 }
